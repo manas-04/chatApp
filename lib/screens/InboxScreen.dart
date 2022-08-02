10e3a9f3e245c0396976737f7e642d4b6804e62b
manas-04 <manasarora038@gmail.com>
2022-08-03 01:56:58 +0530

    Leave Group Feature Added

--- lib/screens/InboxScreen.dart
+++ lib/screens/InboxScreen.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: file_names
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../chats/messages.dart';
 import '../chats/newMessage.dart';
-import '../components/customAppBar.dart';
+import '../components/logOutDialogBox.dart';
 
 class GroupInbox extends StatelessWidget {
   static const String routeName = '/GroupInbox';
@@ -12,16 +14,110 @@ class GroupInbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    late int members;
+    late List<dynamic> membersList;
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
 
     final String groupCode = routeArgs['groupCode'] as String;
     final String groupName = routeArgs['groupName'] as String;
 
+    void _leaveGroup() async {
+      User? user = FirebaseAuth.instance.currentUser;
+
+      var docRef = await FirebaseFirestore.instance
+          .collection('groups')
+          .doc(groupCode)
+          .collection('details')
+          .doc('generalDetails')
+          .get();
+
+      if (docRef.exists) {
+        members = docRef.get('noOfPeople') as int;
+        membersList = docRef.get('groupMember') as List<dynamic>;
+        if (membersList.contains(user!.uid)) {
+          await FirebaseFirestore.instance
+              .collection('groups')
+              .doc(groupCode)
+              .collection('details')
+              .doc('generalDetails')
+              .update({
+            "noOfPeople": members - 1,
+            "groupMember": FieldValue.arrayRemove([user.uid])
+          });
+
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .update({
+            "groups": FieldValue.arrayRemove([groupCode])
+          });
+        }
+        Navigator.pop(context);
+      }
+    }
+
     return Scaffold(
-      appBar: CustomAppbar(
-        backButton: true,
-        title: groupName,
+      appBar: AppBar(
+        title: Text(groupName),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) {
+              if (value == 'search') {
+              } else if (value == 'leaveGroup') {
+                _leaveGroup();
+              } else if (value == 'logout') {
+                showDialog(
+                  context: context,
+                  builder: (context) => const LogOutDialogBox(),
+                );
+              }
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 'search',
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Search'),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Leave Group'),
+                  ],
+                ),
+                value: 'leaveGroup',
+              ),
+              PopupMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.logout_rounded,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Logout'),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
       body: Column(
         children: [
--- lib/screens/joinGroupScreen.dart
+++ lib/screens/joinGroupScreen.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: file_names
-import 'package:chat_app/screens/InboxScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 import '../components/customAppBar.dart';
+import '../screens/InboxScreen.dart';
 
 class JoinScreen extends StatefulWidget {
   static const String routeName = "/JoinGroupScreen";
@@ -92,94 +93,96 @@ class _JoinScreenState extends State<JoinScreen> {
       body: SizedBox(
         width: double.infinity,
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.08,
-              ),
-              Image.asset(
-                'dev_assets/beeLogoAdaptive.png',
-                height: 300,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Enter the group code below :",
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+          child: Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.12,
                 ),
-              ),
-              Form(
-                key: _formKey,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 7,
-                    horizontal: 60,
-                  ),
-                  child: TextFormField(
-                    key: const ValueKey('code'),
-                    decoration: InputDecoration(
-                      labelText: 'Group Code',
-                      prefixIcon: const Icon(Icons.groups_rounded),
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 20,
-                      ),
-                      hintText: "Enter Code",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    onSaved: (value) {
-                      code = value!;
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Group Code is required.";
-                      } else if (value.length > 7 || value.length < 7) {
-                        return "Group Code must be of 7 characters.";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
+                Image.asset(
+                  'dev_assets/beeLogoAdaptive.png',
+                  height: 300,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              if (!_isLoading)
-                MaterialButton(
-                  height: 45,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    side: const BorderSide(color: Colors.white),
-                  ),
-                  elevation: 2,
-                  color: const Color.fromARGB(255, 197, 166, 252),
-                  onPressed: _findGroup,
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'Press to Initiate \nthe Group Joining process.',
-                      softWrap: true,
-                      overflow: TextOverflow.fade,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "Enter the group code below :",
+                    style: TextStyle(
+                      fontSize: 18,
                     ),
                   ),
                 ),
-              if (_isLoading)
-                const Center(
-                  child: CircularProgressIndicator(),
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 7,
+                      horizontal: 60,
+                    ),
+                    child: TextFormField(
+                      key: const ValueKey('code'),
+                      decoration: InputDecoration(
+                        labelText: 'Group Code',
+                        prefixIcon: const Icon(Icons.groups_rounded),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 20,
+                        ),
+                        hintText: "Enter Code",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      onSaved: (value) {
+                        code = value!;
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Group Code is required.";
+                        } else if (value.length > 7 || value.length < 7) {
+                          return "Group Code must be of 7 characters.";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
                 ),
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+                if (!_isLoading)
+                  MaterialButton(
+                    height: 45,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      side: const BorderSide(color: Colors.white),
+                    ),
+                    elevation: 2,
+                    color: const Color.fromARGB(255, 197, 166, 252),
+                    onPressed: _findGroup,
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Join Group >>',
+                        softWrap: true,
+                        overflow: TextOverflow.fade,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                if (_isLoading)
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
